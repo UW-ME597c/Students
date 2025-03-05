f20cad0ad777e28cbac57a6d1cf863ad3c571bd7
Minghao <minghao.ning@uwaterloo.ca>
2025-03-05 01:01:08 -0500

    upload labThree

--- planner.py
+++ planner.py
@@ -1,31 +1,44 @@
-# Type of planner
+
+from math import sin,cos, atan2,atan
+import math
+
 POINT_PLANNER=0; TRAJECTORY_PLANNER=1
 
-
+PARABOLA=0; SIGMOID=1
 
 class planner:
-    def __init__(self, type_):
+
+    def __init__(self, type_, mapName="room"):
 
         self.type=type_
+        self.mapName=mapName
 
     
-    def plan(self, goalPoint=[-1.0, -1.0]):
+    def plan(self, startPose, endPose):
         
         if self.type==POINT_PLANNER:
-            return self.point_planner(goalPoint)
+            return self.point_planner(endPose)
+        
         
         elif self.type==TRAJECTORY_PLANNER:
-            return self.trajectory_planner()
+            return self.trajectory_planner(startPose, endPose)
+        
+
+    def point_planner(self, endPose):
+        return (endPose[0], endPose[1])
 
 
-    def point_planner(self, goalPoint):
-        x = goalPoint[0]
-        y = goalPoint[1]
-        return x, y
-
-    # TODO Part 6: Implement the trajectories here
-    def trajectory_planner(self):
-        pass
-        # the return should be a list of trajectory points: [ [x1,y1], ..., [xn,yn]]
-        # return 
-
+    def trajectory_planner(self, startPose, endPose):
+        # Temporary trajectory planner
+        # For lab3, we only need to connect the start and end points with a straight line in the resolution of 0.2
+        interpolation_resolution=0.2
+        x0, y0 = startPose[0], startPose[1]
+        x1, y1 = endPose[0], endPose[1]
+        dx, dy = x1-x0, y1-y0
+        distance = math.sqrt(dx*dx + dy*dy)
+        steps = int(distance/interpolation_resolution)
+        interpolated_points = []
+        for i in range(steps):
+            interpolated_points.append([x0 + dx/steps*i, y0 + dy/steps*i])
+        return interpolated_points
+    
